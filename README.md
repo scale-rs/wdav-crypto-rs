8fa6029a41fd8fd8689d7ea7b00457c7560b8190
niuhuan <niuhuancn@outlook.com>
2023-03-31 18:09:35 +0800

    :sparkles: Configuration args

--- README.md
+++ README.md
@@ -3,7 +3,36 @@ WDAV
 
 Quick start a wdav server
 
+# 📦️ Install
+
 ```shell
 cargo install wdav
-wdav
+```
+# 📙 Examples
+
+## 🌐 Start a webdav server
+
+```shell
+wdav.exe
+```
+```text
+current dir : D:\Developments\Projects\wdav
+listening on 0.0.0.0:8080 serving .
+```
+
+## 🦮 Print help
+
+```shell
+wdav.exe
+```
+```text
+Quick start a webdav server
+
+Usage: wdav.exe [OPTIONS]
+
+Options:
+  -f, --folder <FOLDER>    Attach to webdav root [default: .]
+  -a, --address <ADDRESS>  Address of listen [default: 0.0.0.0]
+  -p, --port <PORT>        Port of listen [default: 8080]
+  -h, --help               Print help
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
+use std::ffi::OsString;
+use std::net::{IpAddr, SocketAddr};
+
+use clap::Parser;
 use dav_server::warp::dav_dir;
-use std::net::SocketAddr;
+
+#[derive(Debug, Parser)]
+#[command(name = "wdav")]
+#[command(about = "Quick start a webdav server", long_about = None)]
+struct Cli {
+    #[arg(short, long, default_value = ".", help = "Attach to webdav root")]
+    folder: OsString,
+    #[arg(short, long, default_value = "0.0.0.0", help = "Address of listen")]
+    address: OsString,
+    #[arg(short, long, default_value_t = 8080, help = "Port of listen")]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
-    let dir = ".";
-    let addr: SocketAddr = ([0, 0, 0, 0], 8080).into();
-    println!("listening on {:?} serving {}", addr, dir);
-    let warp_dav = dav_dir(dir, true, true);
-    warp::serve(warp_dav).run(addr).await;
+    let args = Cli::parse();
+    match args {
+        Cli {
+            folder,
+            address,
+            port,
+        } => {
+            let cd = std::env::current_dir().unwrap();
+            let path = folder.to_string_lossy().to_string();
+            let addr: IpAddr = address.to_string_lossy().to_string().parse().unwrap();
+            let addr = SocketAddr::new(addr, port);
+            println!("current dir : {}", cd.to_string_lossy().to_string());
+            println!("listening on {} serving {}", addr, path);
+            let warp_dav = dav_dir(path, true, true);
+            warp::serve(warp_dav).run(addr).await;
+        }
+    }
 }
